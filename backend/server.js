99275dbc1422ea33fb69b839213c7fad47487c87
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-03-27 12:14:38 +0200

    create connection to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('Welcome');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { MONGO_URL, PORT } = process.env;
+app.use(express.json());
+
+const main = async () => {
+  await mongoose.connect(MONGO_URL);
+
+  app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}`);
+    console.log(`Localhost:${PORT}`);
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
