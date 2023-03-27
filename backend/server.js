1df907de344206b59efed5d1402b2f26582765ee
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-03-27 12:58:57 +0200

    create basic get request handling on server

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,16 @@ require('dotenv').config();
 const { MONGO_URL, PORT } = process.env;
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 const main = async () => {
   await mongoose.connect(MONGO_URL);
 
   app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
-    console.log(`Localhost:${PORT}`);
+    console.log(`http://127.0.0.1:${PORT}`);
   });
 };
 
