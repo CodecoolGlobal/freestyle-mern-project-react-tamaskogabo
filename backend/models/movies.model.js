01a2967fafcf67f21fc4944b429e63323f3ac216
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-03-27 14:16:22 +0200

    populate server with movies data

--- backend/models/movies.model.js
+++ backend/models/movies.model.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const MovieSchema = new Schema({
-  name: String,
-  genre: String,
-  year: Number,
-}, {timestamps : true});
+const MovieSchema = new Schema(
+  {
+    title: String,
+    genres: [String],
+    year: Number,
+    runtime: Number,
+    release_date: String,
+    writers: [String],
+    actors: [String],
+    storyline: String,
+    directors: [String],
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model('Movie', MovieSchema);
