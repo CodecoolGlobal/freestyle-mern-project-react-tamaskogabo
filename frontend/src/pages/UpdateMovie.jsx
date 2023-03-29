9e75dc3570cf9831dcb80d18418d0ca011c5e710
Pisti332 <varga.istvan.p1@gmail.com>
2023-03-29 13:36:45 +0200

    fix update button

--- frontend/src/pages/UpdateMovie.jsx
+++ frontend/src/pages/UpdateMovie.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import MovieForm from '../Components/MovieForm/MovieForm';
 
 export default function UpdateMovie() {
   const { state } = useLocation();
+  const handlePatch = (event) => {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData(event.target));
+    console.log(formData);
+  };
   console.log(state);
-  return <div>UpdateMovie</div>;
+  return <div>
+    <MovieForm onSubmit={handlePatch} movie={state} />
+  </div>;
 }
