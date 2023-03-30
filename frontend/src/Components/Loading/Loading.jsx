8a6b6cd23465b493b7ebc69a88c0e4b4c55e6a73
Gabor Tamasko <117508570+tamaskogabo@users.noreply.github.com>
2023-03-30 10:30:36 +0200

    create loading animation

---
+++ frontend/src/Components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loading.css';
+
+export default function Loading() {
+  return (
+    <div id='loader-div'>
+      <span className='loader'></span>
+    </div>
+  );
+}
